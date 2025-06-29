fd8632190738cf41d38337a71d4dd16672de7470
ali-shakeel-dev <muhammadalishakeel77@gmail.com>
2025-06-29 18:45:33 +0500

    Navbar Changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
 import PrimaryButton from "./PrimaryButton"
+import { FiExternalLink } from "react-icons/fi";
 
 function Navbar() {
   return (
@@ -10,7 +11,7 @@ function Navbar() {
             <span className="hidden sm:block ml-3 text-xl">ZenNotes</span>
           </a>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            {/* <a className="mr-5 hover:text-gray-900">First Link</a> */}
+            <a href="https://github.com/ali-shakeel-dev/zen-notes/blob/main/README.md" target="blank" className="mr-5 hover:text-gray-900 flex flex-wrap items-center text-base justify-center">README <FiExternalLink className="w-4 h-4 inline ml-1" /></a>
           </nav>
           <PrimaryButton value="Contact" link="https://github.com/ali-shakeel-dev" />
         </div>
